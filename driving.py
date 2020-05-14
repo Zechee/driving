1dab687de52e3f341481ba3dcd4343366bcb0dff
Nathan Yin <yinzeqi11@gmail.com>
2020-05-14 15:09:15 +1200

    add China

---
+++ driving.py
@@ -0,0 +1,10 @@
+country = input('Where are you come from: ')
+age = input('How old are you: ')
+age = int(age)
+
+if country == 'China':
+	if age >= 18:
+		print('Congratulations, you can take the driving license test')
+	else:
+		print('Sorry, you connot take the driving license test')
+
